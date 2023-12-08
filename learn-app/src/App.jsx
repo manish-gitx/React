46de70fd3e761f64cb97bbf02e42e358585774e9
manish-gitx <108692670+manish-gitx@users.noreply.github.com>
2023-12-08 22:24:15 +0530

    final

--- learn-app/src/App.jsx
+++ learn-app/src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import Tag1 from "./Tag1";
+import Tag1 from "./components/Tag1";
+import F from "./components/Footer"
 
 function App(){
     return(
         <div>
-            <p>my name is arth</p>
+            <F />
             <Tag1 />
         </div>
     )
---
+++ learn-app/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+function F(){
+    return( 
+        <div>
+    <h1>hi pakka</h1>
+    <h1>hi tu</h1>
+        </div>
+   
+    )
+}
+export default F;
