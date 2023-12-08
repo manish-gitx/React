54cde241de185de7632b64c7683a6fe5189ade6e
manish-gitx <108692670+manish-gitx@users.noreply.github.com>
2023-12-08 20:36:20 +0530

    push

---
+++ learn-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Tag1 from "./Tag1";
+
+function App(){
+    return(
+        <div>
+            <p>my name is arth</p>
+            <Tag1 />
+        </div>
+    )
+}
+export default App;
